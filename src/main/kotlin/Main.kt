147a616a531a54c434bbd15e589e9cc90f6137ff
Gustavo Wagner <gugawag@gmail.com>
2024-02-23 10:27:40 -0300

    inicio-varias funcionalidades

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,98 @@
+package br.ifpb.pdm
+
+fun main() {
+    val repositorioAnimal = RepositorioAnimal()
+    var opcao = 0
+    while (opcao != 6) {
+        menu()
+        print("Digite a opção: ")
+        opcao = readlnOrNull()?.toInt() ?: 0
+        when (opcao) {
+            0 -> {
+                val animal = Animal(10)
+                animal.nome = "genérico"
+                repositorioAnimal.adicionar(animal)
+            }
+            1 -> {
+                val cachorro = Cachorro(10)
+                cachorro.nome = "Rex"
+                repositorioAnimal.adicionar(cachorro)
+            }
+            2 -> {
+                val gato = Gato(5)
+                gato.nome = "Felix"
+                repositorioAnimal.adicionar(gato)
+            }
+            3 -> {
+                val passaro = Passaro(2)
+                passaro.nome = "Piu"
+                repositorioAnimal.adicionar(passaro)
+            }
+            4 -> {
+                repositorioAnimal.listar()
+            }
+            5 -> {
+                repositorioAnimal.animais.forEach(Animal::emitirSom)
+                repositorioAnimal.animais.forEach { it.emitirSom()}
+            }
+        }
+
+    }
+}
+
+open class Animal(var idade: Int) {
+    open var nome: String = ""
+        get() = "Animal: $field"
+        set(valor) {
+            field = valor
+        }
+
+    open fun emitirSom() {
+        println("Som de animal")
+    }
+}
+
+class Cachorro(idade: Int) : Animal(idade) {
+    override var nome: String = ""
+        get() = "$field"
+        set(valor) {
+            field = valor
+        }
+    override fun emitirSom() {
+        println("Au au")
+    }
+}
+class Gato(idade: Int) : Animal(idade) {
+    override fun emitirSom() {
+        println("Miau")
+    }
+}
+
+class Passaro(idade: Int) : Animal(idade) {
+    override fun emitirSom() {
+        println("Piu piu")
+    }
+}
+
+fun menu() {
+    println("0 - Animal")
+    println("1 - Cachorro")
+    println("2 - Gato")
+    println("3 - Pássaro")
+    println("4 - Listar Animais")
+    println("5 - Emitir som")
+    println("6 - Sair")
+}
+
+class RepositorioAnimal {
+    val animais: MutableList<Animal> = mutableListOf()
+
+    fun adicionar(animal: Animal) {
+        animais.add(animal)
+    }
+
+    fun listar() {
+        animais.forEach { println(it.nome) }
+    }
+}
+
