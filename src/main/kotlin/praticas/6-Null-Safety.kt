22a1da26ae8242dd249c143fe52990960cbdebd6
Luiz Manoel <dantasluizmanoel@gmail.com>
2024-03-09 18:46:56 -0300

    Safe Calls

--- src/main/kotlin/praticas/6-Null-Safety.kt
+++ src/main/kotlin/praticas/6-Null-Safety.kt
@@ -1,16 +1,19 @@
 package br.ifpb.pdm.praticas
 
-class Atividade(val titulo: String, val descricao: String?){
+class Atividade(val titulo: String, val descricao: String?, val data: Data?){
     override fun toString(): String {
         return "Atividade = $titulo || Descrição = $descricao"
     }
 }
+class Data(val dia: Int?, val mes: String?  )
 
 fun listar(repositorio: MutableList<Atividade>){
     for (atividade in repositorio){
         println(atividade)
     }
 }
+
+// fun listarTituloEMes(repositorio: MutableList<Atividade>){}
 
 fun qtdAtividades(repositorio: MutableList<Atividade>){
    val qtdAtividades = repositorio.size
@@ -19,13 +22,14 @@ fun qtdAtividades(repositorio: MutableList<Atividade>){
 
 fun main(){
     val repositorioAtividades = mutableListOf<Atividade>()
-    repositorioAtividades.add(Atividade("Fazer Compras","Café, açúcar, arroz"))
-    repositorioAtividades.add(Atividade("Prova de BD2",null))
-    repositorioAtividades.add(Atividade("Organizar documentos para solicitar férias","Café, açúcar, arroz"))
+    repositorioAtividades.add(Atividade("Fazer Compras","Café, açúcar, arroz", Data(14, "Março")))
+    repositorioAtividades.add(Atividade("Prova de BD2",null, Data(23, null)))
+    repositorioAtividades.add(Atividade("Organizar documentos para solicitar férias","Café, açúcar, arroz", null))
 
     //repositorioAtividades.add(Atividade(null,"Organizar seminário"))
 
     listar(repositorioAtividades)
     qtdAtividades(repositorioAtividades)
+    //listarTituloEMes(repositorioAtividades)
 
 }
