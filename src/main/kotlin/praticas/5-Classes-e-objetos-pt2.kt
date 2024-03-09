c0d8f69354efd9e9867b0b0d3d9d38f4c2d150ed
Caio André Guilherme de Miranda <caio.miranda@vsoft.com.br>
2024-03-09 00:16:54 -0300

    Adicionando a atividade prática

---
+++ src/main/kotlin/praticas/5-Classes-e-objetos-pt2.kt
@@ -0,0 +1,32 @@
+/** SISTEMA DE REGISTROS DE FUNCIONÁRIOS
+ *
+ * Desenvolva um sistema de registro de funcionários para uma empresa.
+ * O sistema deverá permitir registrar diferentes tipos de funcionários e apresentar suas informações.
+ *
+ * Alguns passos abaixo:
+ *
+ * 1.Crie uma classe base chamada Funcionario com as seguintes propriedades:
+ * - Nome (string): o nome do funcionário.
+ * - Idade (int): a idade do funcionário.
+ *
+ * 2.Crie uma classe Gerente que herda da classe Funcionario e adicione uma propriedade adicional:
+ * - Setor (string): o setor em que o gerente trabalha.
+ *
+ * 3.Crie uma classe Desenvolvedor que herda da classe Funcionario e adicione uma propriedade adicional:
+ * - Linguagem (string): a linguagem de programação que o desenvolvedor utiliza.
+ *
+ * 4.Crie uma classe Analista que herda da classe Funcionario e adicione uma propriedade adicional:
+ * - Area (string): a área de especialização do analista.
+ *
+ * 5.Imprima uma mensagem informando que um novo funcionário foi registrado, juntamente com o nome e a idade do
+ * funcionário. (Lembre-se do init)
+ *
+ * 6. Crie um método na classe Funcionario chamado Apresentar que imprima uma mensagem de apresentação do funcionário,
+ * incluindo o nome e a idade.
+ *
+ * 7. Crie uma lista de funcionários e adicione diferentes tipos de funcionários (gerentes, desenvolvedores e analistas)
+ * à lista.
+ *
+ * 8. Utilize o typecast (is e as) para verificar o tipo de cada funcionário na lista e chamar o método Apresentar
+ * correspondente.
+ */
