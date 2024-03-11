695b4a04ccb21554e02d14c47cb97e9cded7d76d
Amandacdev <cruz.amanda@academico.ifpb.edu.br>
2024-03-10 21:40:33 -0300

    Prática e ajustes

--- src/main/kotlin/praticas/6-Null-Safety.kt
+++ src/main/kotlin/praticas/6-Null-Safety.kt
@@ -2,10 +2,33 @@ package br.ifpb.pdm.praticas
 
 class Atividade(val titulo: String, val descricao: String?, val data: Data?){
     override fun toString(): String {
-        return "Atividade = $titulo || Descrição = $descricao"
+        return "Atividade = $titulo || Descrição = $descricao || Data = $data"
     }
 }
-class Data(val dia: Int?, val mes: String?  )
+class Data(val dia: Int?, val mes: String?  ){
+    override fun toString(): String {
+        return "$dia/$mes"
+    }
+}
+
+fun cadastrarAtividade(repositorio: MutableList<Atividade>){
+
+    print("Digite o título da atividade: ")
+    val titulo = readln()
+
+    print("Digite a descrição da atividade: ")
+    val descricao = readln()
+
+    print("Digite o dia da atividade: ")
+    val dia = readlnOrNull()?.toInt()
+
+    print("Digite o mes da atividade: ")
+    val mes = readln()
+    val data = Data(dia, mes)
+
+    repositorio.add(Atividade(titulo,descricao,data))
+    println("\nCadastrado com sucesso!\n")
+}
 
 fun listar(repositorio: MutableList<Atividade>){
     for (atividade in repositorio){
@@ -14,6 +37,8 @@ fun listar(repositorio: MutableList<Atividade>){
 }
 
 // fun listarTituloEMes(repositorio: MutableList<Atividade>){}
+
+// fun listarAtividadeSemData(repositorio: MutableList<Atividade>)
 
 fun qtdAtividades(repositorio: MutableList<Atividade>){
    val qtdAtividades = repositorio.size
@@ -28,8 +53,23 @@ fun main(){
 
     //repositorioAtividades.add(Atividade(null,"Organizar seminário"))
 
-    listar(repositorioAtividades)
-    qtdAtividades(repositorioAtividades)
-    //listarTituloEMes(repositorioAtividades)
+    var opcao = 0
+    while (opcao != 6) {
+        println("1 - Listar atividades")
+        println("2 - Listar quantidade de Atividades")
+        println("3 - Cadastrar Atividade")
+        //println("4 - Listar por título e mês")
+        //println("5 - Lista atividades sem data")
+        println("6 - Sair")
+        print("Digite a opção: ")
+        opcao = readlnOrNull()?.toInt() ?: 8
 
+        when (opcao) {
+            1 -> listar(repositorioAtividades)
+            2 -> qtdAtividades(repositorioAtividades)
+            3 -> cadastrarAtividade(repositorioAtividades)
+            //4 -> listarTituloEMes(repositorioAtividades)
+            //5 -> listarAtividadeSemData(repositorio: MutableList<Atividade>)
+        }
+    }
 }
