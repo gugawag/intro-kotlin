717a481d1fa9ff124c4a771eafca9a43458cf3c8
SimonLucena <112531561+SimonLucena@users.noreply.github.com>
2024-03-14 23:49:54 -0300

    Add files via upload

---
+++ src/main/kotlin/praticas/8-Programacao-funcional.kt
@@ -0,0 +1,24 @@
+// Questão 1
+
+// Questão 2
+class Turma(var nome: String, var notas:MutableList<Double>){
+    // Crie a função AdicionarNotas(lista:MutableList<Double>)
+
+    // Crie a função MediaNotas()
+
+
+    override fun toString(): String {
+        return "Turma(Nome=$nome, Notas=$notas)\n"
+    }
+}
+
+fun main() {
+    // Questão 1
+
+    // Questão 2
+
+    // Questão 3
+
+    // Questão 4
+
+}
